208889de50f63d463ca8c547455948519c5c2f13
kalbii <mathieuwhang587@riversideunified.org>
2023-10-16 14:17:23 -0700

    FINISH

---
+++ Prototype 3 Sounds and Effects/Assets/Scripts/DirtTrail.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirtTrail : MonoBehaviour
+{
+    //private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // if (playerControllerScript.gameOver = true)
+        //{
+        //    Destroy(gameObject);
+        //}
+    }
+}
--- Prototype 3 Sounds and Effects/Assets/Scripts/PlayerController.cs
+++ Prototype 3 Sounds and Effects/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
+    private AudioSource playerAudio;
 
     public float JumpForce = 10.0f;
     public float GravityMod = 0.0f;
@@ -12,21 +14,32 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver = false;
 
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         //playerRb.AddForce(Vector3.up * 1000);
         Physics.gravity *= GravityMod;
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
             isOnGround = false;
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -36,9 +49,15 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         }   else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+            explosionParticle.Play();
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
             Debug.Log("Game Over!");
         }
     }
